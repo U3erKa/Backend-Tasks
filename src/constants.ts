9dae59580656e2dfd3b2d2767e83dad68ff837ff
Viktor Stepanov <vitya.zap@gmail.com>
2022-12-29 14:46:10 +0200

    Add book routes

--- src/constants.ts
+++ src/constants.ts
@@ -2,4 +2,4 @@ import path from 'path';
 
 export const BASE_URL = 'http://localhost';
 export const PORT = 3000;
-export const BOOKS_PATH = path.join(__dirname, 'data', 'books.json');
+export const BOOKS_PATH = path.join(__dirname, '..', 'data', 'books.json');
--- src/routes/booksRouter.ts
+++ src/routes/booksRouter.ts
@@ -1,7 +1,13 @@
 import { Router } from 'express';
-import { jsonParser } from '../middleware/booksMW';
+import { createBook, deleteBook, getBook, getBooks, updateBook } from '../controller/bookController';
+import { jsonParser, validateBook } from '../middleware/booksMW';
+
 const router = Router();
 
 router.use(jsonParser);
+
+router.route('/').get(getBooks).post(validateBook, createBook);
+
+router.route('/:bookId').get(getBook).put(validateBook, updateBook).delete(deleteBook);
 
 export default router;
