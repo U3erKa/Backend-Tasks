aa0b5839cc048a14060dd5ec07602217b02d06f3
Viktor Stepanov <vitya.zap@gmail.com>
2023-01-23 23:48:36 +0200

    Implement primitive error handler

--- app.ts
+++ app.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import { errorHandler } from './errorHandlers';
 import router from './router';
 const app = express();
 
 app.use(router);
+app.use(errorHandler);
 
 export default app;
