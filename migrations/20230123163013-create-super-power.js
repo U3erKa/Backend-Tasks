34fa00ec13a7792f911901dfe2a4351b21884019
Viktor Stepanov <vitya.zap@gmail.com>
2023-01-23 19:01:51 +0200

    Add super_powers table

---
+++ migrations/20230123163013-create-super-power.js
@@ -0,0 +1,33 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('super_powers', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      superPower: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        unique: true,
+        field: 'super_power',
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: 'created_at',
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: 'updated_at',
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('super_powers');
+  },
+};
