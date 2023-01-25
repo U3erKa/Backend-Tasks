bafe24289098b4b1d648e3fc2b3db6ed9ea7f31d
Viktor Stepanov <vitya.zap@gmail.com>
2023-01-25 14:59:39 +0200

    Create images table

---
+++ migrations/20230124211426-create-images.js
@@ -0,0 +1,43 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('images', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      heroId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: 'hero_id',
+        references: {
+          key: 'id',
+          model: { tableName: 'super_heroes' },
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      },
+      path: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: 'created_at',
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: 'updated_at',
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('images');
+  },
+};
