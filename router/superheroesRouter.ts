bda7fd6b860982c26b4a7571346a92dd7ea1c611
Viktor Stepanov <vitya.zap@gmail.com>
2023-01-25 15:01:46 +0200

    Add route for images

--- router/superheroesRouter.ts
+++ router/superheroesRouter.ts
@@ -1,4 +1,8 @@
 import { Router } from 'express';
+import multer from 'multer';
+import path from 'path';
+
+import { addImagesToHero } from '../controller/imageController';
 import {
   createSuperHero,
   deleteSuperHero,
@@ -9,7 +13,23 @@ import {
 
 const superheroesRouter = Router();
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.resolve(__dirname, '../../public/images'));
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage });
+
 superheroesRouter.route('/').get(getSuperHeroes).post(createSuperHero);
-superheroesRouter.route('/:heroId').get(getSuperHero).put(updateSuperHero).delete(deleteSuperHero);
+superheroesRouter
+  .route('/:heroId')
+  .get(getSuperHero)
+  .post(upload.array('images'), addImagesToHero)
+  .put(updateSuperHero)
+  .delete(deleteSuperHero);
 
 export default superheroesRouter;
